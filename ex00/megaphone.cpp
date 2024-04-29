58388db05c128e8e8ba154b4a7cf7cc679867f28
angela <angelabarrio2000@gmail.com>
2024-04-29 10:25:02 +0200

    ejerciccio terminado

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,18 +6,32 @@
 /*   By: angela <angela@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 11:37:39 by angela            #+#    #+#             */
-/*   Updated: 2024/04/21 19:54:53 by angela           ###   ########.fr       */
+/*   Updated: 2024/04/29 10:24:23 by angela           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-// using namespace std;
+#include <cctype>
 
-int main()
+int main(int argc, char *argv[])
 {
-    std::cout << "UwU";
-    std::cout << "UwU" << std::endl;
-    
+    std::string str;
 
+    if (argc == 1)
+    {
+        str = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+        std::cout << str << std::endl;
+        return (0);
+    }
+    for (int i = 1; argv[i] != NULL; i++)
+    {
+        for (int j = 0; argv[i][j] != '\0'; j++)
+        {
+            argv[i][j] = toupper(argv[i][j]);
+            std::cout << argv[i][j];
+        }
+        std::cout << " ";
+    }
+    std::cout << std::endl;
     return (0);
 }
