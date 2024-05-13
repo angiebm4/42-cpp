0a8917b4fb11040961b5ba9e2f5d6f8dbc93143c
Angela Barrio Magdalena <abarrio-@c1r4s2.42madrid.com>
2024-05-13 10:48:48 +0200

    arreglado ctrl d exx01

--- cpp_00/ex01/main.cpp
+++ cpp_00/ex01/main.cpp
@@ -3,17 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: angela <angela@student.42.fr>              +#+  +:+       +#+        */
+/*   By: abarrio- <abarrio-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 11:30:11 by abarrio-          #+#    #+#             */
-/*   Updated: 2024/05/04 00:02:32 by angela           ###   ########.fr       */
+/*   Updated: 2024/05/13 10:44:36 by abarrio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.hpp"
-
-/* TODO: control d capear */
-/* TODO: no funciona al añadir mas de 8 contactos :( */
 
 int main(void)
 {
@@ -24,11 +21,14 @@ int main(void)
     i = 0;
     while (1)
     {
+        if (std::cin.eof())
+            return(0);
         std::cout << "TO CONTINUE ENTER:" << std::endl;
         std::cout << "ADD -> IF YOU WANT TO ADD A NEW CONTACT" << std::endl;
         std::cout << "SEARCH -> IF YOU WANT TO SEARCH FOR ANY CONTACT" << std::endl;
         std::cout << "EXIT -> IF YOU WANT TO GO OUT THE PHONEBOOK" << std::endl;
         getline(std::cin, type);
+        
         if (type == "EXIT" || type == "exit")
             return (0);
         else if (type == "SEARCH" || type == "search")
@@ -38,6 +38,8 @@ int main(void)
             ph.add(i);
             i++;
         }
+        if (i == 8)
+            i = 0;
     }
     return (0);
 }
