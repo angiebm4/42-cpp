a433f383ca20222744d14bd309a9826ed14217e1
Angela Barrio Magdalena <abarrio-@c2r4s1.42madrid.com>
2024-05-16 12:58:10 +0200

    cambio de compilador a c++ inicio ex5cpp1

--- cpp_01/ex04/main.cpp
+++ cpp_01/ex04/main.cpp
@@ -3,16 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: angela <angela@student.42.fr>              +#+  +:+       +#+        */
+/*   By: abarrio- <abarrio-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 13:24:03 by angela            #+#    #+#             */
-/*   Updated: 2024/05/11 12:51:32 by angela           ###   ########.fr       */
+/*   Updated: 2024/05/16 12:55:02 by abarrio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cstdlib>
 #include <fstream>
+
 
 void	leaks(void)
 {
@@ -24,18 +25,35 @@ int main(int argc, char *argv[])
     atexit(leaks);
     if (argc != 4)
     {
-        std::cout << "invalid args pls follow the following sintax: [filename] [sentence 1] [sentence 2]" << std::endl;
+        std::cerr << "Invalid arguments. Please follow the syntax: [filename] [sentence 1] [sentence 2]" << std::endl;
         return(0);
     }
-    std::ofstream    file;
-    file.open(argv[1], );
+    std::ifstream    file;
+    file.open(argv[1]);
     if (file.fail())
+        std::cerr << "error al abrir el archivo" << std::endl;
+    std::ofstream   file2;
+    std::string     str = argv[2];
+    str.append(".replace");
+    file2.open(str.c_str());
+    if (file2.fail())
+        std::cerr << "error al abrir el archivo" << std::endl;
+    if (file.is_open())
     {
-        std::cout << "error al"
+        std::string     s1 = argv[3];
+        std::string line;
+        while(file.good())
+        {
+            std::getline(file, line);
+            size_t find = line.find(argv[3]);
+            if (find != std::string::npos)
+            {
+                line.erase(find, find + s1.length());
+            }
+        }
     }
-
-    file << argv[2] << std::endl;
+    file2 << "hola que tal" << std::endl;
     file.close();
-
+    file2.close();
     return (0);
 }
