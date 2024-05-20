f084e30455bf51b4df0599fd3cae50bb57df5828
angela <angelabarrio2000@gmail.com>
2024-05-20 11:22:26 +0200

    finish

--- cpp_01/ex04/main.cpp
+++ cpp_01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: angela <angela@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 13:24:03 by angela            #+#    #+#             */
-/*   Updated: 2024/05/17 10:36:50 by angela           ###   ########.fr       */
+/*   Updated: 2024/05/20 11:15:07 by angela           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,18 @@
 #include <cstdlib>
 #include <fstream>
 
-
-void	leaks(void)
+void    replace_word(std::ifstream &file, std::ofstream &file2, const std::string &s1, const std::string &s2)
 {
-	system("leaks -q sed_losers");
-}
-
-void    replace_word(std::ifstream &file, std::ofstream &file2, char *argv[])
-{
-
-    std::string s1 = argv[2];
     std::string line;
     
-    while(file.good())
+    while(std::getline(file, line))
     {
-        std::getline(file, line);
-        size_t find = 0;
-        while(line.find(argv[2]))
+        size_t find = line.find(s1);
+        while(find != std::string::npos)
         {
-            if (find != std::string::npos)
-            {
-                line.erase(find, s1.length());
-                file2 << line.insert(find, argv[3]) << std::endl;
-                find += s1.length();
-            }
-            
+            line.erase(find, s1.length());
+            line.insert(find, s2);
+            find = line.find(s1, find + s2.length());
         }
         file2 << line << std::endl;
     }
@@ -46,7 +33,6 @@ void    replace_word(std::ifstream &file, std::ofstream &file2, char *argv[])
 
 int main(int argc, char *argv[])
 {
-    atexit(leaks);
     if (argc != 4)
     {
         std::cerr << "Invalid arguments. Please follow the syntax: [filename] [sentence 1] [sentence 2]" << std::endl;
@@ -55,17 +41,22 @@ int main(int argc, char *argv[])
     
     std::ifstream    file;
     std::ofstream   file2;
-    std::string     str = argv[1];
+    std::string     file_name = argv[1];
     
     file.open(argv[1]);
-    if (file.fail())
+    if (!file.is_open())
+    {
         std::cerr << "error opening file" << std::endl;
-    str.append(".replace");
-    file2.open(str.c_str());
-    if (file2.fail())
+        return (0);
+    }
+    file_name.append(".replace");
+    file2.open(file_name.c_str());
+    if (!file2.is_open())
+    {
         std::cerr << "error opening file" << std::endl;
-    if (file.is_open())
-        replace_word(file, file2 , argv);
+        return (0);
+    }
+    replace_word(file, file2 , argv[2], argv[3]);
     file.close();
     file2.close();
     return (0);
