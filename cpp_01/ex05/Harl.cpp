74d22ea1bb971e34c2bb5abe65eaa7a90d2769b0
angela <angelabarrio2000@gmail.com>
2024-05-27 13:28:12 +0200

    end

--- cpp_01/ex05/Harl.cpp
+++ cpp_01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: angela <angela@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 10:54:12 by angela            #+#    #+#             */
-/*   Updated: 2024/05/21 11:27:42 by angela           ###   ########.fr       */
+/*   Updated: 2024/05/26 12:39:19 by angela           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,25 +24,41 @@ Harl::~Harl(void)
 
 void    Harl::debug(void)
 {
-    
+    std::cout << "<< DEBUG >>" << std::endl;
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
 }
 
 void    Harl::info(void)
 {
-    
+    std::cout << "<< INFO >>" << std::endl;
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn t put enough bacon in my burger! If you did, I wouldn t be asking for more!" << std::endl;
 }
 
 void    Harl::warning(void)
 {
-    
+    std::cout << "<< WARNING >>" << std::endl;
+    std::cout << "I think I deserve to have some extra bacon for free. I ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void    Harl::error(void)
 {
-    
+    std::cout << "<< ERROR >>" << std::endl;
+    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
 void    Harl::complain(std::string level)
 {
+    void (Harl::*method[4])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+    std::string strLevel[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    int i = 0;
     
+    while (i < 4)
+    {
+        if (level == strLevel[i])
+        {
+            (this->*method[i])();
+            break ;
+        }
+        i++;
+    }
 }
--- cpp_01/ex05/main.cpp
+++ cpp_01/ex05/main.cpp
@@ -6,19 +6,21 @@
 /*   By: angela <angela@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 13:24:03 by angela            #+#    #+#             */
-/*   Updated: 2024/05/21 10:53:30 by angela           ###   ########.fr       */
+/*   Updated: 2024/05/26 12:37:23 by angela           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include "harl.hpp"
 
-void	leaks(void)
-{
-	system("leaks -q harl");
-}
 
-int main(int argc, char *argv[])
+int main(void)
 {
-    atexit(leaks);
+    Harl    Harl;
+    
+    Harl.complain("DEBUG");
+    Harl.complain("INFO");
+    Harl.complain("WARNING");
+    Harl.complain("ERROR");
     return (0);
 }
