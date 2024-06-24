21b860d814ffe38cb8a194f12d341c1a233d4b80
angiebm4 <angelabarrio2000@gmail.com>
2024-06-24 19:12:02 +0200

    cosas

--- cpp_03/ex00/ClapTrap.cpp
+++ cpp_03/ex00/ClapTrap.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ClapTrap.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: angela <angela@student.42.fr>              +#+  +:+       +#+        */
+/*   By: abarrio- <abarrio-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 15:31:40 by angela            #+#    #+#             */
-/*   Updated: 2024/06/23 16:22:31 by angela           ###   ########.fr       */
+/*   Updated: 2024/06/24 11:25:40 by abarrio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,23 @@ ClapTrap::ClapTrap(std::string name)
 {
     _name = name;
     _health = 10;
-    _energy = 5;
+    _energy = 10;
     _aaDamage = 0;
     std::cout << "ClapTrap is boorn us a guy called " << _name << std::endl;
 }
 
 ClapTrap::~ClapTrap(void)
 {
-    std::cout << _name << " die" << std::endl;
+    std::cout << "ClapTrap " << _name << " has been destroyed" << std::endl;
 }
 
 void ClapTrap::attack(const std::string& target)
 {
+    if (this->_health <= 0)
+    {
+        std::cout << "ClapTrap " << _name << " is death" << std::endl;
+        return;
+    }
     if (this->_energy > 0)
     {
         std::cout << "ClapTrap " << _name << " attacks " << target <<
@@ -40,27 +45,35 @@ void ClapTrap::attack(const std::string& target)
 
 void ClapTrap::takeDamage(unsigned int amount)
 {
-    std::cout << _name << " is been attack " << amount << " of damage have taken" << std::endl;
+    if (this->_health <= 0)
+    {
+        std::cout << "ClapTrap " << _name << " is death" << std::endl;
+        return;
+    }
+    std::cout << "ClapTrap " << _name << " is been attack " << amount << " of damage have taken" << std::endl;
     _health -= amount;
     if (_health <= 0)
     {
-        std::cout << _name << " die" << std::endl;   
+        std::cout << "ClapTrap " << _name << " die" << std::endl;   
     }
 }
 
 void ClapTrap::beRepaired(unsigned int amount)
 {
+    if (this->_health <= 0)
+    {
+        std::cout << "ClapTrap " << _name << " is death" << std::endl;
+        return;
+    }
     if (this->_energy > 0)
     {
         std::cout << "ClapTrap " << _name << " health " << amount <<
              " of life" << std::endl;
         this->_energy -= 1;
         this->_health += amount;
-        std::cout << "Now " << _name << " have " << _health << " of health" << std::endl;
+        std::cout << "ClapTrap " << _name << " have " << _health << " of health" << std::endl;
     }
     else
-        std::cout << _name << " is exausted let her rest" << std::endl;
+        std::cout << "ClapTrap " << _name << " is exausted let her rest" << std::endl;
     
 }
-
-
