b8668383fbd5e3897e1258eab409c16f484c13a7
angiebm4 <angelabarrio2000@gmail.com>
2024-10-24 12:20:26 +0200

    end cpp02 , fix some functions

--- cpp_02/ex02/Fixed.cpp
+++ cpp_02/ex02/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: abarrio- <abarrio-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 12:18:14 by angela            #+#    #+#             */
-/*   Updated: 2024/09/18 15:22:37 by abarrio-         ###   ########.fr       */
+/*   Updated: 2024/10/24 12:18:19 by abarrio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,29 +53,152 @@ Fixed::~Fixed(void)
     std::cout << "Destructor called" << std::endl;
 }
 
+/* Comparison operators */
+
+bool Fixed::operator>(const Fixed& obj)
+{
+    return (this->fp_number > obj.fp_number);
+}
+
+bool Fixed::operator<(const Fixed& obj)
+{
+    return (this->fp_number < obj.fp_number);
+}
+
+bool Fixed::operator>=(const Fixed& obj)
+{
+    return (this->toFloat() >= obj.fp_number);
+}
+
+bool Fixed::operator<=(const Fixed& obj)
+{
+    return (this->fp_number <= obj.fp_number);
+}
+
+bool Fixed::operator==(const Fixed& obj)
+{
+    return (this->fp_number == obj.fp_number);
+}
+
+bool Fixed::operator!=(const Fixed& obj)
+{
+    return (this->fp_number != obj.fp_number);
+}
+
+/* Arithmetic operators */
+
+Fixed Fixed::operator+(const Fixed& obj)
+{
+    Fixed   Result(this->toFloat() + obj.toFloat());
+
+    return (Result);
+}
+
+Fixed Fixed::operator-(const Fixed& obj)
+{
+    Fixed   Result(this->toFloat() - obj.toFloat());
+
+    return (Result);
+}
+
+Fixed Fixed::operator*(const Fixed& obj)
+{
+    Fixed   Result(this->toFloat() * obj.toFloat());
+
+    return (Result);
+}
+
+Fixed Fixed::operator/(const Fixed& obj)
+{
+    Fixed   Result(this->toFloat() / obj.toFloat());
+
+    return (Result);
+}
+
+/* Increment/decrement operators */
+
+Fixed Fixed::operator++(int) /* i++ */
+{
+    Fixed result = *this;
+
+    this->fp_number++;
+
+    return (result);
+}
+
+Fixed& Fixed::operator++(void) /* ++i */
+{
+    this->fp_number++;
+    return (*this);
+}
+
+Fixed Fixed::operator--(int) /* i-- */
+{
+    Fixed result = *this;
+
+    this->fp_number--;
+
+    return (result);
+}
+
+Fixed& Fixed::operator--(void) /* --i */
+{
+    this->fp_number--;
+    return (*this);
+}
+
+
 /*-----------------------------------------------------------------------*/
 
 /* convierte un int en float dividiendo ntr  2^Fractional_Bits*/
-float Fixed::toFloat( void ) const
+float Fixed::toFloat(void) const
 {
     return (float)fp_number / (1 << Fractional_Bits);
 }
 
-/* convirte de float a int moviendo los bits a la derecha por fractional:bits*/
-int Fixed::toInt( void ) const
+/* convirte de float a int moviendo los bits a la derecha por fractional_bits*/
+int Fixed::toInt(void) const
 {
     return fp_number >> Fractional_Bits;
 }
 
 void Fixed::setRawBits(int const raw)
 {
-    std::cout << raw << std::endl;
+    this->fp_number = raw;
 }
 
 int Fixed::getRawBits(void) const
 {
     std::cout << "getRawBits member function called" << std::endl;
     return (fp_number);
+}
+
+Fixed& Fixed::min(Fixed& fixed_1, Fixed& fixed_2)
+{
+    if (fixed_1.toFloat() < fixed_2.toFloat())
+        return (fixed_1);
+    return (fixed_2);
+}
+
+const Fixed& Fixed::min(const Fixed& fixed_1, const Fixed& fixed_2)
+{
+    if (fixed_1.toFloat() < fixed_2.toFloat())
+        return (fixed_1);
+    return (fixed_2);
+}
+
+Fixed& Fixed::max(Fixed& fixed_1, Fixed& fixed_2)
+{
+    if (fixed_1.toFloat() > fixed_2.toFloat())
+        return (fixed_1);
+    return (fixed_2);
+}
+
+const Fixed& Fixed::max(const Fixed& fixed_1, const Fixed& fixed_2)
+{
+    if (fixed_1.toFloat() > fixed_2.toFloat())
+        return (fixed_1);
+    return (fixed_2);
 }
 
 std::ostream& operator<<(std::ostream &COUT, const Fixed &obj)
--- cpp_02/ex02/Fixed.hpp
+++ cpp_02/ex02/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: abarrio- <abarrio-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 12:18:12 by angela            #+#    #+#             */
-/*   Updated: 2024/09/18 15:04:12 by abarrio-         ###   ########.fr       */
+/*   Updated: 2024/10/24 12:14:11 by abarrio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,15 +35,42 @@ class Fixed
         /* copy asigment operator */
         Fixed& operator=(const Fixed& obj);
 
+        /* Comparison operators */
+        bool operator>(const Fixed& obj);
+        bool operator<(const Fixed& obj);
+        bool operator>=(const Fixed& obj);
+        bool operator<=(const Fixed& obj);
+        bool operator==(const Fixed& obj);
+        bool operator!=(const Fixed& obj);
+
+        /* Arithmetic operators */
+        Fixed operator+(const Fixed& obj);
+        Fixed operator-(const Fixed& obj);
+        Fixed operator*(const Fixed& obj);
+        Fixed operator/(const Fixed& obj);
+
+        /* Increment/decrement operators */
+        Fixed operator++(int); /* i++ */
+        Fixed& operator++(void); /* ++i */
+        Fixed operator--(int); /* i-- */
+        Fixed& operator--(void); /* --i */
+
         ~Fixed(void);
 
         int getRawBits(void) const;
         void setRawBits(int const raw);
 
-        float toFloat( void ) const;
-        int toInt( void ) const;
-        
+        float toFloat(void) const;
+        int toInt(void) const;
+
+        static Fixed& min(Fixed&, Fixed&);
+        static const Fixed& min(const Fixed&, const Fixed&);
+        static Fixed& max(Fixed&, Fixed&);
+        static const Fixed& max(const Fixed&, const Fixed&);
+
 };
-    
+/* Al sobrecargar este operador permite que puedas imprimir directamente una clase */
+/* utilizando la salida estandar COUT llamamos a tofloat para que sea el numero */
+/* de punto flotante casteado a float el que se imprime */
 std::ostream& operator<<(std::ostream &COUT, const Fixed &obj);
 #endif
