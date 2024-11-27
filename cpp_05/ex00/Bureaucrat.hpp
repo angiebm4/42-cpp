10fc442a7c592504b8d7339e67549726e35cc0a3
Angela Barrio <angelabarrio2000@gmail.com>
2024-11-27 10:40:31 +0100

    first

---
+++ cpp_05/ex00/Bureaucrat.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 09:16:42 by marvin            #+#    #+#             */
+/*   Updated: 2024/11/25 09:16:42 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+
+class Bureaucrat
+{
+    private:
+        const std::string   _name;
+
+};
+
+# endif
